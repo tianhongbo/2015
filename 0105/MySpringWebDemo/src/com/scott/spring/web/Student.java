42fc96ef8daa24ca42a25a8bd756aed1bc19fdbb
tianhongbo <tianhongbo1@gmail.com>
2015-01-05 21:52:15 -0800

    Try Spring MVC framework

---
+++ 0105/MySpringWebDemo/src/com/scott/spring/web/Student.java
@@ -0,0 +1,32 @@
+package com.scott.spring.web;
+
+public class Student {
+	private Integer age;
+	private String name;
+	private Integer id;
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	public Integer getAge() {
+		return this.age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+
+}
